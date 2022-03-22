4a44c7e6efedec2cace541228dc082a3ed65d707
Leo Araujo <leonardo_02452@hotmail.com>
2022-03-22 15:23:46 -0300

    Criada função renderWithRouter e testes para componente App (req1)

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,52 @@
-test('x', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testa o componente App', () => {
+  it('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+    const favPokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favPokemonsLink).toBeInTheDocument();
+  });
+  it(`Testa se a aplicação é redirecionada para a página inicial,
+  na URL / ao clicar no link Home da barra de navegação`, () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  it(`Testa se a aplicação é redirecionada para a página de About,
+  na URL /about, ao clicar no link About da barra de navegação`, () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  it(`Testa se a aplicação é redirecionada para a página de Pokémons Favoritados,
+  na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação`, () => {
+    const { history } = renderWithRouter(<App />);
+    const favPokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    userEvent.click(favPokemonsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+  it(`Testa se a aplicação é redirecionada para a página Not Found
+  ao entrar em uma URL desconhecida`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina-inexistente');
+    const notFoundTitle = screen
+      .getByRole('heading', { name: /page requested not found/i });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+});
