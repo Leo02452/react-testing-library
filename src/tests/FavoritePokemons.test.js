ce07dd04486e0a08d09c3df86d268a574b5a3de6
Leo Araujo <leonardo_02452@hotmail.com>
2022-03-22 16:27:29 -0300

    Criados os testes para o componente FavoritePokemons (req3)

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testa o componente FavoritePokemons', () => {
+  it(`Testa se é exibido na tela a mensagem 'No favorite pokemon found',
+  se a pessoa não tiver pokémons favoritos`, () => {
+    renderWithRouter(<App />);
+    const favLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favLink);
+    const favMessagem = screen.getByText('No favorite pokemon found');
+    expect(favMessagem).toBeInTheDocument();
+  });
+  it('Testa se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: 'More details' });
+
+    userEvent.click(moreDetails);
+
+    const favCheckbox = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(favCheckbox);
+
+    const favLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favLink);
+
+    const favPokemons = screen.getAllByTestId(/pokemon-name/i);
+    expect(favPokemons).toHaveLength(1);
+  });
+});
