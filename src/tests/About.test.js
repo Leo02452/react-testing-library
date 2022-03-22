68c1a99623ee92c2b38b41b8cc51d5b6407a586e
Leo Araujo <leonardo_02452@hotmail.com>
2022-03-22 16:03:11 -0300

    Criados os testes para o componente About (req2)

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testa o componente About', () => {
+  // it('Testa se a página contém as informações sobre a Pokédex', () => {
+  //   renderWithRouter(<App />);
+  //   const aboutLink = screen.getByRole('link', { name: 'About' });
+  //   userEvent.click(aboutLink);
+  // });
+  it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+    const aboutTitle = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+  it('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+
+    userEvent.click(aboutLink);
+
+    const firstParagraph = screen.getByText(/this application simulates a Pokédex/i);
+    const secondParagraph = screen.getByText(/one can filter Pokémons by type/i);
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+  });
+  it('Teste se a página contém uma imagem especifica de uma Pokédex', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+
+    userEvent.click(aboutLink);
+
+    const favPokemonsLink = screen.getByRole('img', { role: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png' });
+
+    expect(favPokemonsLink).toBeInTheDocument();
+    expect(favPokemonsLink).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
